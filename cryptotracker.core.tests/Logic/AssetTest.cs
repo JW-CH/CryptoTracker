6a98e088c5f075116acea16093cf4b40968b64dc
JW-CH <17313367+JW-CH@users.noreply.github.com>
2025-12-08 17:17:33 +0100

    basic testing projects

---
+++ cryptotracker.core.tests/Logic/AssetTest.cs
@@ -0,0 +1,16 @@
+﻿namespace cryptotracker.core.tests.Logic;
+
+[TestFixture]
+public class AssetTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+}
